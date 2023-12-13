58b0f80a2d61b52ab265242529d7c99d0b980b6e
shahriar <smshahriarkabir@gmail.com>
2023-12-13 21:59:54 +0600

    add: specific product quantity field disabled in cart

--- simple-variation-swatches-custom.php
+++ simple-variation-swatches-custom.php
@@ -22,3 +22,19 @@ define( 'SVSWP_PATH', plugin_dir_path( SVSWP ) );
 
 include( SVSWP_PATH . 'includes/core-data.php');
 include( SVSWP_PATH . 'includes/class/admin/class-svswpro-admin-loader.php');
+
+function disable_quantity_field_on_cart_page( $quantity, $cart_item_key ) {
+    $cart = WC()->cart->get_cart();
+    $cart_item = $cart[$cart_item_key];
+    $product_id = $cart_item['product_id'];
+
+    // Specify the product IDs for which you want to disable the quantity field
+    $products_to_disable = array( 1020, 1109 ); // Replace with your product IDs
+
+    if ( in_array( $product_id, $products_to_disable ) ) {
+        $quantity = sprintf( '<div class="quantity"><input type="number" class="input-text qty text" step="1" min="1" max="" name="cart[%s][qty]" value="%s" title="Qty" size="4" inputmode="numeric" readonly style="pointer-events: none;"/></div>', $cart_item_key, $cart_item['quantity'] );
+    }
+
+    return $quantity;
+}
+add_filter( 'woocommerce_cart_item_quantity', 'disable_quantity_field_on_cart_page', 10, 2 );
